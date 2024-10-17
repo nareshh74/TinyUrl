d875c503cca8eb07245ce96d1c8fb67b425044b0
Naresh <nareshh74@gmail.com>
2024-10-18 00:11:55 +0530

    Use repository pattern (#5)
    * moved state to controller to make domain stateless.
    1 issue with this - controllers have transient life - so actually states ae not retained in subsequent requests
    so tests are failing - move it to repository in next commit
    
    * added repo pattern
    
    * state has to be removed in logic/domain layer
    
    ---------
    
    Co-authored-by: Naresh Kumar <kumarnaresh@microsoft.com>

---
+++ TinyUrl/IUrlRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace TinyUrl;
+
+public interface IUrlRepository
+{
+    Task<bool> TryGetCodeAsync(Uri url, out string? code, CancellationToken _);
+    Task<bool> TryGetUrlAsync(string code, out Uri url, CancellationToken _);
+    Task<bool> TryAddUrlCodeMappingAsync(Uri url, string code, CancellationToken _);
+}
